a23892b1eaaaaa4c89c9e141d6439550f801d64d
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-01 18:32:07 +0100

    Initial commit

---
+++ src/main/java/de/memozone/Coffee2Go/Coffee2GoApplication.java
@@ -0,0 +1,13 @@
+package de.memozone.Coffee2Go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coffee2GoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coffee2GoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/memozone/Coffee2Go/Coffee2GoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.memozone.Coffee2Go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coffee2GoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
